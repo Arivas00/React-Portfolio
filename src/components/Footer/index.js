4d3545dd5fb505b15bf640a3c1513c8caf934865
Alex <a.chisar00@gmail.com>
2021-06-13 15:46:23 -0400

    images added and about me page done

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,22 +9,32 @@ export default function Navbar() {
         textDecoration: "none",
     }
 
+    const text = {
+        color: "white"
+    }
+
     return (
         <div >
             <footer className="fixed-bottom bg-dark">
                 <Row className="justify-content-center">
-                    <Col lg={1} style={icons}>
+                    <Col sm={1} style={icons}  >
                         <a href="https://github.com/Arivas00"><i className="fab fa-github"></i></a>
                     </Col>
-                    <Col lg={1} style={icons}>
+                    <Col sm={1} style={icons}>
                         <a href="https://www.linkedin.com/in/alex-chisar/"><i className="fab fa-linkedin"></i></a>
                     </Col>
-                    <Col lg={1} style={icons}>
+                    <Col sm={1} style={icons}>
                         <a href="mailto:a.chisar00@gmail.com"><i className="far fa-envelope"></i></a>
                     </Col>
                     <Col lg={1} style={icons}>
                         <a href="https://drive.google.com/file/d/19QlZs6WRAUCi7K-xb_Dk7CIttAAR_E6l/view?usp=sharing"><i className="far fa-file"></i></a>
                     </Col>
+                </Row>
+                <Row className="justify-content-center">
+                    <Col sm={1} style={text}><p>Github</p></Col>
+                    <Col sm={1} style={text}><p>LinkedIn</p></Col>
+                    <Col sm={1} style={text}><p>Email</p></Col>
+                    <Col sm={1} style={text}><p>Resume</p></Col>
                 </Row>
             </footer>
         </div>
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,43 @@
 import React from "react";
+import { Container, Card, Image, Row, Col } from 'react-bootstrap';
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import '../assets/styles/style.css';
+import ProfilePic from "../assets/images/Prof_pic.jpg"
 
 export default function About() {
+
+   const imgStyle = {
+        width: '250px',
+        display: "block",
+        marginLeft: "auto",
+        marginRight: "auto"
+    }
+
+
     return (
         <div >
-            <Navbar/>
-            <h1>About Page</h1>
-            <Footer/>
+            <Navbar />
+            <br></br>
+            <br></br>
+            <Container>
+                <Row className="justify-content-center">
+                    <Col lg={6}>
+                        <Card style={{ width: '40rem' }}>
+                            <Card.Body>
+                                <Image src={ProfilePic} style={imgStyle} />
+                                <br></br>
+                                <h1 style={{ textAlign: "center" }}>About Me</h1>
+                                <hr></hr>
+                                <Card.Text>
+                                    Hi my name is Alex Chisar and welcome to my portfolio website! A little about myself, I graduated from Temple University with a bachelors degree in neuroscience in 2010. The past 5 years I have been working at a non-profit called Bancroft NeuroRehab where I provide cognitive therapy to people with traumatic brain injuries. I enrolled in the UPenn Bootcamp looking for a career change in something I have always been interested in. CODING!
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+            <Footer />
         </div>
 
     )
