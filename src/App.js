16deb0499653e33f129b7a451cc50dec40e47943
Alex <a.chisar00@gmail.com>
2021-06-12 22:21:06 -0400

    Routes setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Router>
+        <Route exact path="/">
+          <About />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+        <Route exact path="/portfolio">
+          <Portfolio />
+        </Route>
+      </Router>
     </div>
   );
 }
